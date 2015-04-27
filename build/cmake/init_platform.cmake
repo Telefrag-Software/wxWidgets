c8bc385ee7b3eb3d3f878d9e05a6a531369fb108
Alexander Lamaison <alexander.lamaison@gmail.com>
2015-04-27 15:22:41 +0100

    Fix forcing clang into C++11 mode.
    Must not use list(APPEND ...).  If CMAKE_CXX_FLAGS already contains something, it inserts semicolons into the command line which breaks everything.

--- build/cmake/init_platform.cmake
+++ build/cmake/init_platform.cmake
@@ -58,7 +58,7 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 	# it thinks <type_traits> is usable.  It can't tell the
 	# difference between usable and available. C++11 mode
 	# makes it usable.
-	list(APPEND CMAKE_CXX_FLAGS "-std=c++0x")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 endif()
 
 
