245ca13768966ac2cadf6e5858007d52d8b6f7cb
Kevin Scroggins <nitro404@gmail.com>
2021-07-23 17:48:35 -0400

    wxWidgets Hunterization

---
+++ build/cmake/hunter/HunterConfig.cmake
@@ -0,0 +1,37 @@
+include_guard()
+
+hunter_config(CatchSelfTest
+	URL "https://github.com/Telefrag-Software/Catch2/archive/refs/heads/hunter-wx-ee4acb6.zip"
+	VERSION "ee4acb6"
+	SHA1 "e53c0a1e77440ee1dbf1d95733de3eff9fdb7f3c"
+)
+
+hunter_config(EXPAT
+	URL "https://github.com/Telefrag-Software/libexpat/archive/refs/heads/hunter-wx-adaa0c3.zip"
+	VERSION "adaa0c3"
+	SHA1 "82140e7c20908f08c659439f46dc6c45a8e6c479"
+)
+
+hunter_config(JPEG
+	URL "https://github.com/Telefrag-Software/libjpeg-turbo/archive/refs/heads/hunter-wx-8524936.zip"
+	VERSION "8524936"
+	SHA1 "b135f44ecafa08c91cd60ac5433e6d4fa5dfc990"
+)
+
+hunter_config(PNG
+	URL "https://github.com/Telefrag-Software/libpng/archive/refs/heads/hunter-wx-7cc7524be.zip"
+	VERSION "7cc7524be"
+	SHA1 "aa01ecbaa389a2c5eddfd931b6759fef336f02e0"
+)
+
+hunter_config(TIFF
+	URL "https://github.com/Telefrag-Software/libtiff/archive/refs/heads/hunter-wx-28ce1ef.zip"
+	VERSION "28ce1ef"
+	SHA1 "6e57cf6d8ae52fc695fb5bd046dd3baac65598b9"
+)
+
+hunter_config(WXZLIB
+	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-wx-786a2d7.zip"
+	VERSION "786a2d7"
+	SHA1 "b916771c96412d1853b44207d09e9b14d904ce91"
+)
