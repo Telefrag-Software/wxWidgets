03647472f910b1c0f6a40474c23e0ae5285c2680
Kevin Scroggins <nitro404@gmail.com>
2022-04-24 13:31:25 -0400

    Disable LibTIFF

--- build/cmake/hunter/HunterConfig.cmake
+++ build/cmake/hunter/HunterConfig.cmake
@@ -1,23 +1,23 @@
 include_guard()
 
 # LibTIFF Dependencies
-hunter_config(lzma
-    URL "https://github.com/Telefrag-Software/lzma/archive/refs/heads/hunter-2.5.3.zip"
-    VERSION "2.5.3"
-    SHA1 "c541a20b4c5bd9283d83be28b1582ce233a2683e"
-)
-
-hunter_config(ZLIB
-    URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.11.1.zip"
-    VERSION "1.2.11.1"
-    SHA1 "974d834f2e00dc647703c25f14617379986abd02"
-)
-
-hunter_config(zstd
-    URL "https://github.com/Telefrag-Software/zstd/archive/refs/heads/hunter-1.5.0.zip"
-    VERSION "1.5.0"
-    SHA1 "724c091ed2c6ae6e5219111a75d021c42d41b892"
-)
+#hunter_config(lzma
+#    URL "https://github.com/Telefrag-Software/lzma/archive/refs/heads/hunter-2.5.3.zip"
+#    VERSION "2.5.3"
+#    SHA1 "c541a20b4c5bd9283d83be28b1582ce233a2683e"
+#)
+#
+#hunter_config(ZLIB
+#    URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.11.1.zip"
+#    VERSION "1.2.11.1"
+#    SHA1 "974d834f2e00dc647703c25f14617379986abd02"
+#)
+#
+#hunter_config(zstd
+#    URL "https://github.com/Telefrag-Software/zstd/archive/refs/heads/hunter-1.5.0.zip"
+#    VERSION "1.5.0"
+#    SHA1 "724c091ed2c6ae6e5219111a75d021c42d41b892"
+#)
 
 # wxWidgets Dependencies
 hunter_config(CatchSelfTest
@@ -46,15 +46,15 @@ hunter_config(png
         BUILD_SHARED_LIBS=OFF
 )
 
-hunter_config(TIFF
-    URL "https://github.com/Telefrag-Software/libtiff/archive/refs/heads/hunter-wx-bbe71f4a.zip"
-    VERSION "bbe71f4a"
-    SHA1 "9fbf9da30df13dbb15ea1019945c589f44710d4a"
-    CMAKE_ARGS
-        BUILD_SHARED_LIBS=OFF
-        lzma=OFF
-        zstd=OFF
-)
+#hunter_config(TIFF
+#    URL "https://github.com/Telefrag-Software/libtiff/archive/refs/heads/hunter-wx-bbe71f4a.zip"
+#    VERSION "bbe71f4a"
+#    SHA1 "9fbf9da30df13dbb15ea1019945c589f44710d4a"
+#    CMAKE_ARGS
+#        BUILD_SHARED_LIBS=OFF
+#        lzma=OFF
+#        zstd=OFF
+#)
 
 hunter_config(WXZLIB
     URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-wx-d0982b8.zip"
