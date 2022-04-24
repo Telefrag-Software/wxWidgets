4bda2d0aeb04bde1227a9fc0ec382c255526b199
Kevin Scroggins <nitro404@gmail.com>
2022-04-24 17:11:24 -0400

    Add ZLIB

--- build/cmake/hunter/HunterConfig.cmake
+++ build/cmake/hunter/HunterConfig.cmake
@@ -2,22 +2,23 @@ include_guard()
 
 # LibTIFF Dependencies
 #hunter_config(lzma
-#    URL "https://github.com/Telefrag-Software/lzma/archive/refs/heads/hunter-2.5.3.zip"
-#    VERSION "2.5.3"
-#    SHA1 "c541a20b4c5bd9283d83be28b1582ce233a2683e"
-#)
-#
-#hunter_config(ZLIB
-#    URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.11.1.zip"
-#    VERSION "1.2.11.1"
-#    SHA1 "974d834f2e00dc647703c25f14617379986abd02"
+#	URL "https://github.com/Telefrag-Software/lzma/archive/refs/heads/hunter-2.5.3.zip"
+#	VERSION "2.5.3"
+#	SHA1 "c541a20b4c5bd9283d83be28b1582ce233a2683e"
 #)
 #
 #hunter_config(zstd
-#    URL "https://github.com/Telefrag-Software/zstd/archive/refs/heads/hunter-1.5.0.zip"
-#    VERSION "1.5.0"
-#    SHA1 "724c091ed2c6ae6e5219111a75d021c42d41b892"
+#	URL "https://github.com/Telefrag-Software/zstd/archive/refs/heads/hunter-1.5.0.zip"
+#	VERSION "1.5.0"
+#	SHA1 "724c091ed2c6ae6e5219111a75d021c42d41b892"
 #)
+
+# LibTIFF / LibPNG Dependencies
+hunter_config(ZLIB
+	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.11.1.zip"
+	VERSION "1.2.11.1"
+	SHA1 "974d834f2e00dc647703c25f14617379986abd02"
+)
 
 # wxWidgets Dependencies
 hunter_config(CatchSelfTest
