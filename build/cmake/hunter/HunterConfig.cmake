4a495f401507b1d02b2a11a34faa30cd42f8dea3
Kevin Scroggins <nitro404@gmail.com>
2022-04-25 18:27:23 -0400

    Update ZLib

--- build/cmake/hunter/HunterConfig.cmake
+++ build/cmake/hunter/HunterConfig.cmake
@@ -65,5 +65,5 @@ hunter_config(png
 hunter_config(ZLIB
 	URL "https://github.com/Telefrag-Software/zlib/archive/refs/heads/hunter-1.2.12.1.zip"
 	VERSION "1.2.12.1"
-	SHA1 "13fe010169c4cfd3a55e98f6334a322149c5dca4"
+	SHA1 "b41c9126bf63dfa53fbb496e24dcceb30ddabee4"
 )
